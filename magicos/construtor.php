13c7c61f19fc8243993cb859283a03fa8e5f2dc3
Antonio Junior Francis da Silva <juniorfrancis91@gmail.com>
2020-11-22 22:12:56 -0300

    Adicionando métodos mágicos - __SET/__GET/__CALL

--- magicos/construtor.php
+++ magicos/construtor.php
@@ -3,5 +3,11 @@
 require_once "../autoload/autoload-psr4.php";
 
 $cli = new App\Classes\Clientes("Junior", "11955807069", 30);
+$cli->cpf = "777.777.777.77";
+$cli->rg = "11.111.111.1";
+
+//echo $cli->cidade;
+
+$cli->alterar('Junior Francis', 20);
 
 var_dump($cli);
--- src/Classes/Clientes.php
+++ src/Classes/Clientes.php
@@ -15,7 +15,36 @@ class Clientes
       $this->telefone = $telefone;
       $this->idade = $idade;
 
-  
+   }
+   /*A função GET__ é utlizada como tratamento para quando chamamos alguma propriedade
+   em que ainda não foi instanciada*/
+   public function __get(string $nomePropriedade)
+   {
+      return "A propriedade não existe";
+   }
+
+   /*O Método "__SET" nos permite criar propriedades de cclasses dinamicamente, e as mesmas só 
+   aparecerão caso frem tratadas (if, else).*/
+   public function __set(string $nomePropriedade, $valorPropriedade)
+   {
+      if ($nomePropriedade === 'cpf')
+      {
+         $this->cpf = \str_replace('.', '', $valorPropriedade);
+      }
+      else
+      {
+         $this->$nomePropriedade = $valorPropriedade;
+      } 
+
+   }
+
+   public function __call(string $nomeMetodo, array $argumentoMetodo)
+   {
+      if ($nomeMetodo === 'alterar')
+      {
+         $this->nome = $argumentoMetodo[0];
+         $this->idade = $argumentoMetodo[1];
+      }
    }
 
    public function Comprar(): void
