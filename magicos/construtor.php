3b0d5a78e09fff2c432699ae51837db367589f69
Antonio Junior Francis da Silva <juniorfrancis91@gmail.com>
2020-11-22 21:05:12 -0300

    Adicionando métodos construtores

---
+++ magicos/construtor.php
@@ -0,0 +1,7 @@
+<?php
+
+require_once "../autoload/autoload-psr4.php";
+
+$cli = new App\Classes\Clientes("Junior", "11955807069", 30);
+
+var_dump($cli);
--- src/Classes/Clientes.php
+++ src/Classes/Clientes.php
@@ -9,6 +9,15 @@ class Clientes
    public string $endereco;
    public string $telefone;
 
+   public function __construct(string $nome, string $telefone, int $idade)
+   {
+      $this->nome = $nome;
+      $this->telefone = $telefone;
+      $this->idade = $idade;
+
+  
+   }
+
    public function Comprar(): void
    {
       echo "O {$this->nome} realizou uma compra!<br>";
